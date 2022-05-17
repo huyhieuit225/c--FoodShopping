7a8d96a1f88ef85342cc4115133986c243b9940f
huyhieuit225 <66211132+huyhieuit225@users.noreply.github.com>
2022-05-18 05:45:46 +0700

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using PRN292_Assignment.DAO;
+using PRN292_Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+
+namespace PRN292_Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Home
+        public ActionResult Index()
+        {
+            List<ProductDetail> listPD = new ProductDetailDAO().GetProduct();
+            ViewData["ProductList"] = listPD;
+            ProductCategoryDAO pcdb = new ProductCategoryDAO();
+            List<ProductCategory> listPC = pcdb.GetProductCategory();
+            ViewData["ProductCategoryList"] = listPC;
+            return View();
+        }
+    }
+}
